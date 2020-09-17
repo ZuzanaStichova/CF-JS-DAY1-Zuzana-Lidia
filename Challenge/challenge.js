f5f1b3bd7fb58d8dae3890950bdaba33fcf409ba
Lidia Cekic <lidiacekic9@gmail.com>
2020-09-17 14:09:58 +0200

    challenge2 complete

--- Challenge/challenge.js
+++ Challenge/challenge.js
@@ -7,19 +7,19 @@
 ];*/
 
 var city_array = [
-["Vienna","is","a","nice","city"],
-["Vienna","is","a","nice","city"],
-["Vienna","is","a","nice","city"],
-["Vienna","is","a","nice","city"],
-["Vienna","is","a","nice","city"],
+  ['Vienna', 'is', 'a', 'nice', 'city'],
+  ['Vienna', 'is', 'a', 'nice', 'city'],
+  ['Vienna', 'is', 'a', 'nice', 'city'],
+  ['Vienna', 'is', 'a', 'nice', 'city'],
+  ['Vienna', 'is', 'a', 'nice', 'city'],
 ];
 
-console.table(city_array);
+// console.table(city_array);
 
-for (i=0; i<=4; i++)
-for (j=0; j<=4; j++)
-{
-	document.write(city_array[i][j])
-/*	document.write(city_array[i][0])*/
-/*	document.write('<br>')*/
-};
+// for (i=0; i<=4; i++)
+// for (j=0; j<=4; j++)
+// {
+// 	document.write(city_array[i][j])
+// /*	document.write(city_array[i][0])*/
+// /*	document.write('<br>')*/
+// };
