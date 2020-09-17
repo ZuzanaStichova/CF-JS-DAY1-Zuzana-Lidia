82f0a9e236e624772109b7b67f7e12231b897ab5
Zuzana Stichova <z.stichova@gmail.com>
2020-09-17 13:35:52 +0200

    Challenge exercise 1

--- Challenge/challenge.js
+++ Challenge/challenge.js
@@ -1,25 +1,27 @@
-/*var city_array = [
-["Vienna","is","a","nice","city"],
-["","is","","",""],
-["","","a","",""],
-["","","","nice",""],
-["","","","","city"]
-];*/
-
-var city_array = [
-["Vienna","is","a","nice","city"],
+var city_array =[ ["Vienna","is","a","nice","city"],
 ["Vienna","is","a","nice","city"],
 ["Vienna","is","a","nice","city"],
 ["Vienna","is","a","nice","city"],
 ["Vienna","is","a","nice","city"],
 ];
 
-console.table(city_array);
+for (i=0; i<=4; i++) {
+	document.write(city_array[i][i] + "<br> <br>");
+}
 
-for (i=0; i<=4; i++)
-for (j=0; j<=4; j++)
-{
-	document.write(city_array[i][j])
-/*	document.write(city_array[i][0])*/
-/*	document.write('<br>')*/
-};
+/*document.write(city_array[0][0])
+document.write('<br>')
+document.write(city_array[1][1])
+document.write('<br>')
+document.write(city_array[2][2])
+document.write('<br>')
+document.write(city_array[3][3])
+document.write('<br>')
+document.write(city_array[4][4])
+document.write('<br>')
+*/
+/*
+for (j=0; j<=4; j++) {
+	document.write(city_array[i][j]);
+	document.write('<br>');
+}*/
